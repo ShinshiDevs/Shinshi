5fa5571319c9c9a064aaf2f80e3efc4e6399ebff
stefanlight <steffmukhin@gmail.com>
2024-02-29 22:48:31 +0300

    some beautiful sh*t will appear here

---
+++ shinshi/__init__.py
@@ -0,0 +1,26 @@
+"""Shinshi module.
+
+Shinshi is a modern and user-friendly Discord bot designed
+to give you and your servers great functionality and stable performance.
+
+!WARNING
+This module cannot be run directly. You must run `shinshi.bot` for correctly initialisation of project.
+"""
+from typing import Final, Sequence
+
+__all__: Sequence[str] = (
+    "__author__",
+    "__maintainers__",
+    "__license__",
+    "__copyright__",
+    "__github__",
+    "__support__"
+)
+
+__author__: Final[str] = "stefanlight"
+__maintainers__: Final[Sequence[str]] = ("stefanlight", "theroer")
+__license__: Final[str] = "GPL-3.0"
+__copyright__: Final[str] = "Copyright (C) 2024-Present Shinshi Developers Team"
+
+__github__: Final[str] = "https://github.com/ShinshiDevs/Shinshi"
+__support__: Final[str] = "https://discord.gg/3bXW7an2ke"
---
+++ shinshi/bot/__init__.py
@@ -0,0 +1,1 @@
+"""Shinshi entry point"""
