1f62009e45c914ba9b658cf7a31bb46cf66a5f17
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2024-09-20 12:32:38 +0300

    feat: ping command

--- shinshi/extensions/general/commands/__init__.py
+++ shinshi/extensions/general/commands/__init__.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
-__all__: Sequence[str] = ("StatisticCommand", "UserCommand", "GuildCommand")
+__all__: Sequence[str] = (
+    "StatisticCommand",
+    "UserCommand",
+    "GuildCommand",
+    "PingCommand",
+)
 
 from collections.abc import Sequence
 
 from .guild_command import GuildCommand
+from .ping_command import PingCommand
 from .stats_command import StatisticCommand
 from .user_command import UserCommand
