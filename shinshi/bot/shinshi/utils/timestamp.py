795acd5681249dc8de27217ca9498e6f29b35712
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2024-11-20 09:22:55 +0300

    Shortened the code and made the determination of the current time in UTC to add timedelta

--- shinshi/bot/shinshi/utils/timestamp.py
+++ shinshi/bot/shinshi/utils/timestamp.py
@@ -1,17 +1,14 @@
-from datetime import datetime, timedelta
+from datetime import UTC, datetime, timedelta
 from typing import Literal, TypeAlias
 
 TimestampStyle: TypeAlias = Literal["t", "T", "d", "D", "f", "F", "R"]
 
 
 def format_timestamp(time: float | int, style: TimestampStyle | None = None) -> str:
-    if style is not None:
-        return f"<t:{int(time)}:{style}>"
-    return f"<t:{int(time)}>"
+    return f'<t:{int(time)}{f":{style}" if style else ""}>'
 
 
 def format_datetime(time: datetime | timedelta, style: TimestampStyle | None = None) -> str:
     if isinstance(time, timedelta):
-        time = datetime.now() + time
-    assert isinstance(time, datetime)
+        time = datetime.now(UTC) + time
     return format_timestamp(time.timestamp(), style)
