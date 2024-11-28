3854f234a6c60255528245c30abd3bd24e3ab6af
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2024-11-28 13:02:30 +0300

    Remove moderation module on some time
    Signed-off-by: stefanlight8 <64615032+stefanlight8@users.noreply.github.com>

--- shinshi/bot/shinshi/extensions/moderation/commands/__init__.py
+++
@@ -1,5 +0,0 @@
-from collections.abc import Sequence
-
-from shinshi.extensions.moderation.commands.kick_command import KickCommand
-
-__all__: Sequence[str] = ("KickCommand",)
--- shinshi/bot/shinshi/extensions/moderation/utils/get_audit_reason.py
+++
@@ -1,6 +0,0 @@
-from hikari.guilds import Member
-
-
-def get_audit_reason(moderator: Member, reason: str) -> str:
-    suffix: str = f"[@{str(moderator)} (ID: {moderator.id})]"
-    return f"{reason} {suffix}"
