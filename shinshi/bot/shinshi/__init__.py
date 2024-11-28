fd0aee682f5cd8a909617252fcca8411025a8954
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2024-11-28 12:53:20 +0300

    Rename `__version__` in `__version_info__`
    Signed-off-by: stefanlight8 <64615032+stefanlight8@users.noreply.github.com>

--- shinshi/bot/shinshi/__init__.py
+++ shinshi/bot/shinshi/__init__.py
@@ -3,5 +3,5 @@ from collections.abc import Sequence
 from shinshi.abc.models.version import VersionInfo
 from shinshi.utils.version import get_version
 
-__version__: VersionInfo = get_version()
-__all__: Sequence[str] = ("__version__",)
+__version_info__: VersionInfo = get_version()
+__all__: Sequence[str] = ("__version_info__",)
