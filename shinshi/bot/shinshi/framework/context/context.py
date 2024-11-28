f08f0d9970ac1471df74244c7946bef6c134f8ba
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2024-11-28 13:05:52 +0300

    Move `shinshi.framework.interaction.context` in `shinshi.framework.context.context`
    Signed-off-by: stefanlight8 <64615032+stefanlight8@users.noreply.github.com>

---
+++ shinshi/bot/shinshi/framework/context/context.py
@@ -0,0 +1,27 @@
+import attrs
+from aurum.context import InteractionContext
+from hikari.embeds import Embed
+
+from shinshi.framework.bot.bot import Bot
+from shinshi.abc.i18n.ilocale import ILocale
+from shinshi.enums.colour import Colour
+
+
+@attrs.define(eq=False, kw_only=True, hash=False, weakref_slot=False)
+class Context(InteractionContext):
+    bot: Bot = attrs.field(repr=False)
+    locale: ILocale = attrs.field(repr=True)
+
+    async def create_warning_response(
+        self, title: str, *, description: str | None = None, ephemeral: bool = True
+    ) -> None:
+        return await self.create_response(
+            embed=Embed(description=description, colour=Colour.YELLOW).set_author(name=title), ephemeral=ephemeral
+        )
+
+    async def create_error_response(
+        self, title: str, *, description: str | None = None, ephemeral: bool = True
+    ) -> None:
+        return await self.create_response(
+            embed=Embed(description=description, colour=Colour.RED).set_author(name=title), ephemeral=ephemeral
+        )
